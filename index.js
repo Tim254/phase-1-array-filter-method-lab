c4506ee0a174bd07254ea7be7e7a92a00ee06f69
Tim254 <tim.mailu@gmail.com>
2022-06-10 16:23:10 +0300

    commit tree b3e7daeb9eb6209a97dbcb866b5018cbff687016

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//  const isMatching = drivers.filter(findMatching)
+ function findMatching(kadere, newKadere) {
+     const isMatching = kadere.filter(
+         driverMatch => driverMatch.toLowerCase() === newKadere.toLowerCase()
+     )
+     return isMatching;
+ }
+ function fuzzyMatch(drivers, letter) {
+    const stringMatch = drivers.filter((driverName) => 
+         driverName.slice(0,2) === letter.slice(0, 2)
+     )
+     return stringMatch;
+ } 
